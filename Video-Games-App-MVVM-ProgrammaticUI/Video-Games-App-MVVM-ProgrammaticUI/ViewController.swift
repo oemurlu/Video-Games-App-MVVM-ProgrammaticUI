8a10cf516e1ad3af7490275a2f03402ab08a5ab6
oemurlu <oemurlu@gmail.com>
2024-01-21 13:18:29 +0300

    add project for init

---
+++ Video-Games-App-MVVM-ProgrammaticUI/Video-Games-App-MVVM-ProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video-Games-App-MVVM-ProgrammaticUI
+//
+//  Created by Osman Emre Ömürlü on 21.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
